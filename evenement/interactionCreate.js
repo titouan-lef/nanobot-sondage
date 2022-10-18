2851441fd535e38d20c8f990c1e6eab23815feca
Titouan Le Floch <titouan.lefloch@orange.fr>
2022-10-18 16:15:44 +0200

    test correction channel privé 4

--- evenement/interactionCreate.js
+++ evenement/interactionCreate.js
@@ -1,3 +1,5 @@
+const { PermissionsBitField } = require('discord.js');
+
 module.exports = (bot, inter) => {
     if (inter.channel.permissionsFor(guild.members.me).has(PermissionsBitField.Flags.ViewChannel))
     {
