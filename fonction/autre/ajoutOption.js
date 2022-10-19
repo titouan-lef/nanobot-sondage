b42e3e5a02e68c02edd3f1b9cb24721bec3e663e
Titouan Le Floch <titouan.lefloch@orange.fr>
2022-10-20 00:17:22 +0200

    bug ajout proposition 26

--- fonction/autre/ajoutOption.js
+++ fonction/autre/ajoutOption.js
@@ -19,6 +19,8 @@ module.exports =
         {
             propositionValide.push(proposition);
 
+            console.log(channel);
+
             let message = channel.messages.fetch(idSondage);
             
             let texte = parametre.texte;
