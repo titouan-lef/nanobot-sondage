71b342159e936c70f138fea996720d18a284d804
Titouan Le Floch <titouan.lefloch@orange.fr>
2022-10-20 00:28:46 +0200

    bug ajout proposition 28

--- fonction/autre/ajoutOption.js
+++ fonction/autre/ajoutOption.js
@@ -39,7 +39,7 @@ module.exports =
             let tabUser = sondage.tabUtilisateur;
             tabUser.forEach(user => user.tabVote.push(objVote.nouveau(idVote, proposition)));
 
-            message.editMessage({content: texte, embeds: [designSondage], components: tabBouton});
+            //message.editMessage({content: texte, embeds: [designSondage], components: tabBouton});
         }
         else
             console.log("le nombre maximum de proposition est déjà atteint");
