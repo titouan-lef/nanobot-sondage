c88f4da87d3cc1c92ab57795263e0ece9c4eee56
Titouan Le Floch <titouan.lefloch@orange.fr>
2022-11-03 02:08:17 +0100

    bug BDD 32

--- fonction/autre/ajoutOption.js
+++ fonction/autre/ajoutOption.js
@@ -3,6 +3,8 @@ const alphabet = constante.getAlphabet();
 
 const fonction = require("../utile.js");
 
+const sondageBDD = require("../../bdd/sondage.js");
+
 module.exports =
 {
     ajoutOption: async (messageProposition, sondage) =>
@@ -29,6 +31,8 @@ module.exports =
 
             let message = await channel.messages.fetch(sondage.id_sondage);
             await message.edit({content: texte, embeds: [designSondage.data], components: tabBouton});
+
+            await sondageBDD.updatePropositionValideEtEmbed(sondage.id_sondage, propositionValide, designSondage);
         }
         else
             console.log("le nombre maximum de proposition est déjà atteint");
